8bde01076344f78a45c4814e586e86f1868bbe47
Agony <gcl1197134883@gmail.com>
2023-02-14 20:33:50 +0800

    first commit with class User

---
+++ src/com/hspedu/generic/homework/Homework02.java
@@ -0,0 +1,49 @@
+package com.hspedu.generic.homework;
+
+/**
+ * @Author Agony
+ * @Create 2023/2/14 20:30
+ * @Version 1.0
+ */
+public class Homework02 {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+class User {
+    private int id;
+    private int age;
+    private String name;
+
+    public User(int id, int age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
